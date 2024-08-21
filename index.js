13e50c7b00e5846e469043b1bfeaaf45a5ac7f32
sesip <sesi_prosper@yahoo.co.uk>
2024-08-21 20:01:48 +0100

    Update

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const express = require("express")
 const app = express()
 const port= 3000
 const router= require('./routes/create-account-routes')
-const { string } = require('joi')
+
 
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.get("/test", (request, response)=>{
-   return response.status(200).json({
-        error: false,message: "Sucess"
+// app.get("/test", (request, response)=>{
+//    return response.status(200).json({
+//         error: false,message: "Sucess"
         
-    })
-})
+//     })
+// })
 app.use('/api', router)
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`)
